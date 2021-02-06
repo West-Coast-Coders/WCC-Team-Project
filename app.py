c17329aa49fac5e8438498494dfaf4905e3493cc
Panda64 <JaylenSchelb@yahoo.com>
2021-02-05 16:42:09 -0800

    Filter function implemented
    Have yet to figure out how to identify which list to be filtered ect...

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 import os
 from flask import Flask, request, render_template, jsonify
 from dotenv import load_dotenv
+from filters import filter_list
 
 load_dotenv()
 
 app = Flask(__name__)
 
+API_info = {
+    'x-rapidapi-key': "SIGN-UP-FOR-KEY",
+    'x-rapidapi-host': "unogsng.p.rapidapi.com"
+    }
+
 @app.route('/')
 def main():
     pass
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ filters.py
@@ -0,0 +1,21 @@
+API_info = {
+    'x-rapidapi-key': "SIGN-UP-FOR-KEY",
+    'x-rapidapi-host': "unogsng.p.rapidapi.com"
+    }
+
+def filter_list(filters: dict, titles_json):
+    url = "https://unogsng.p.rapidapi.com/search"
+    response = requests.request("GET", url, headers=API_info, params=filters)
+    response = response.text
+
+    title_list = []
+    filtered_list = []
+
+    for title in titles_json:
+        title_list.append(title["netflixid"])
+
+    for title in response["results"]:
+        if title["id"] in title_list:
+            filtered_list.append(title["id"])
+
+    return filtered_list
