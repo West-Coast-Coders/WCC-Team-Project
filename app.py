f7c4cc26042b65b6e28f3f4bc614e4346dbeeb6d
Panda64 <JaylenSchelb@yahoo.com>
2021-02-08 12:50:17 -0800

    Filter route progress (not completed)

--- app.py
+++ app.py
@@ -16,6 +16,22 @@ API_info = {
 def main():
     pass
 
+@app.route('/process-filters', methods=['POST'])
+def process():
+    title_type = request.form['type']
+    start_year = request.form['start+year']
+    order_by = request.form['order_by']
+    audiosubtitle_andor = request.form['audiosubtitle_andor']
+    start_rating = request.form['start_rating']
+    end_rating = request.form['end_rating']
+    subtitle = request.form['subtitle']
+    country_list = request.form['country_list']
+    audio = request.form['audio']
+    country_andorunique = request.form['country_andorunique']
+    end_year = request.form['end_year']
+    current_list = request.form['current_list']
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
