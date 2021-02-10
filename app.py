eb285b4cc2862cf3f6ac738b71500022c3b5074e
Panda64 <JaylenSchelb@yahoo.com>
2021-02-10 13:23:43 -0800

    Ready to test filter functionality

--- app.py
+++ app.py
@@ -16,20 +16,34 @@ API_info = {
 def main():
     pass
 
-@app.route('/process-filters', methods=['POST'])
-def process():
-    title_type = request.form['type']
-    start_year = request.form['start+year']
-    order_by = request.form['order_by']
-    audiosubtitle_andor = request.form['audiosubtitle_andor']
-    start_rating = request.form['start_rating']
-    end_rating = request.form['end_rating']
-    subtitle = request.form['subtitle']
-    country_list = request.form['country_list']
-    audio = request.form['audio']
-    country_andorunique = request.form['country_andorunique']
-    end_year = request.form['end_year']
-    current_list = request.form['current_list']
+# @app.route('/process-filters', methods=['POST'])
+# def process():
+#     title_type = request.form['type']
+#     start_year = request.form['start_year']
+#     order_by = request.form['order_by']
+#     audiosubtitle_andor = request.form['audiosubtitle_andor']
+#     start_rating = request.form['start_rating']
+#     end_rating = request.form['end_rating']
+#     subtitle = request.form['subtitle']
+#     country_list = request.form['country_list']
+#     audio = request.form['audio']
+#     country_andorunique = request.form['country_andorunique']
+#     end_year = request.form['end_year']
+#     current_list = request.form['current_list']
+
+#     filters = {
+#         "type":title_type
+#         "start_year":start_year,
+#         "orderby":order_by,
+#         "audiosubtitle_andor":audiosubtitle_andor,
+#         "start_rating":start_rating,
+#         "end_rating":end_rating,
+#         "subtitle":"english",
+#         "countrylist":country_list,
+#         "audio":audio,
+#         "country_andorunique":country_andorunique,
+#         "end_year":end_year
+#     }
 
 
 
