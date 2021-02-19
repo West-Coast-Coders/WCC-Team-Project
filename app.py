e8ef2b6e1a61f6e0cf8adb5427b66bdda469ca66
Gobind Puniani <gobindpuniani@gmail.com>
2021-02-19 14:52:42 -0800

    Updated API key and added new secondary route for title details

--- app.py
+++ app.py
@@ -61,14 +61,28 @@ def get_letter_for_units(units):
     """Returns a shorthand letter for the given units."""
     return 'F' if units == 'imperial' else 'C' if units == 'metric' else 'K'
 
+@app.route('/country-id')
+def countrycode():
+    url = "https://unogsng.p.rapidapi.com/countries"
+
+    headers = {
+        'x-rapidapi-key': "2e473e01d3msh716bf7f4c960569p101e32jsn9d46bb61a5dc",
+        'x-rapidapi-host': "unogsng.p.rapidapi.com"
+        }
+
+    response = requests.request("GET", url, headers=headers)
+
+    print(response.text)
+
 @app.route('/expiring-soon')
 def expiring():
     """Displays results for titles that are expiring soon from Netflix."""
     # Use 'request.args' to retrieve the country code from the query parameters
-    country = request.args.get('countrycode')
+    # country = request.args.get('countrycode')
 
     params = {
-        "countrylist": country,
+        # Use a default country code of 78 for USA
+        "countrylist": "78",
         # For testing purposes, limit number of returned titles to 5
         "limit": 5
     }
@@ -91,6 +105,188 @@ def expiring():
     # pp.pprint(result_json)
 
     return render_template('expirations.html', result_json = result_json, title_details = title_details)
+
+@app.route('/title/<netflixid>')
+def title_details(netflixid):
+    """Displays all the details for an individual title on a full page."""
+    # Send a GET request for the details of a specific title using its unique Netflix ID
+    details = requests.get(url='https://unogsng.p.rapidapi.com/title', params={'netflixid': netflixid}, headers=headers).json()["results"][0]
+    # Send the resulting dictionary to a new page to display the details
+    return render_template('title_details.html', details=details)
+
+
+
+"""
+{
+            "country": "Argentina ",
+            "id": 21,
+            "countrycode": "AR"
+        },
+        {
+            "country": "Australia ",
+            "id": 23,
+            "countrycode": "AU"
+        },
+        {
+            "country": "Belgium ",
+            "id": 26,
+            "countrycode": "BE"
+        },
+        {
+            "country": "Brazil ",
+            "id": 29,
+            "countrycode": "BR"
+        },
+        {
+            "country": "Canada ",
+            "id": 33,
+            "countrycode": "CA"
+        },
+        {
+            "country": "Switzerland ",
+            "id": 34,
+            "countrycode": "CH"
+        },
+        {
+            "country": "Germany ",
+            "id": 39,
+            "countrycode": "DE"
+        },
+        {
+            "country": "France ",
+            "id": 45,
+            "countrycode": "FR"
+        },
+        {
+            "country": "United Kingdom",
+            "id": 46,
+            "countrycode": "GB"
+        },
+        {
+            "country": "Mexico ",
+            "id": 65,
+            "countrycode": "MX"
+        },
+        {
+            "country": "Netherlands ",
+            "id": 67,
+            "countrycode": "NL"
+        },
+        {
+            "country": "Sweden ",
+            "id": 73,
+            "countrycode": "SE"
+        },
+        {
+            "country": "United States",
+            "id": 78,
+            "countrycode": "US"
+        },
+        {
+            "country": "Iceland ",
+            "id": 265,
+            "countrycode": "IS"
+        },
+        {
+            "country": "Japan ",
+            "id": 267,
+            "countrycode": "JP"
+        },
+        {
+            "country": "Portugal ",
+            "id": 268,
+            "countrycode": "PT"
+        },
+        {
+            "country": "Italy ",
+            "id": 269,
+            "countrycode": "IT"
+        },
+        {
+            "country": "Spain ",
+            "id": 270,
+            "countrycode": "ES"
+        },
+        {
+            "country": "Czech Republic ",
+            "id": 307,
+            "countrycode": "CZ"
+        },
+        {
+            "country": "Greece ",
+            "id": 327,
+            "countrycode": "GR"
+        },
+        {
+            "country": "Hong Kong ",
+            "id": 331,
+            "countrycode": "HK"
+        },
+        {
+            "country": "Hungary ",
+            "id": 334,
+            "countrycode": "HU"
+        },
+        {
+            "country": "Israel ",
+            "id": 336,
+            "countrycode": "IL"
+        },
+        {
+            "country": "India ",
+            "id": 337,
+            "countrycode": "IN"
+        },
+        {
+            "country": "South Korea",
+            "id": 348,
+            "countrycode": "KR"
+        },
+        {
+            "country": "Lithuania ",
+            "id": 357,
+            "countrycode": "LT"
+        },
+        {
+            "country": "Poland ",
+            "id": 392,
+            "countrycode": "PL"
+        },
+        {
+            "country": "Romania ",
+            "id": 400,
+            "countrycode": "RO"
+        },
+        {
+            "country": "Russia",
+            "id": 402,
+            "countrycode": "RU"
+        },
+        {
+            "country": "Singapore ",
+            "id": 408,
+            "countrycode": "SG"
+        },
+        {
+            "country": "Slovakia ",
+            "id": 412,
+            "countrycode": "SK"
+        },
+        {
+            "country": "Thailand ",
+            "id": 425,
+            "countrycode": "TH"
+        },
+        {
+            "country": "Turkey ",
+            "id": 432,
+            "countrycode": "TR"
+        },
+        {
+            "country": "South Africa",
+            "id": 447,
+            "countrycode": "ZA"
+        }"""
 
 
 
